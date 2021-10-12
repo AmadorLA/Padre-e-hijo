7daa47b5878eca9e8cb5077431f101f82990b02b
AmadorLA <amadorlopezalejandro1@gmail.com>
2021-10-11 23:28:24 -0500

    Add project files.

---
+++ Padre e hijo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padre_e_hijo
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto2D a;
+            a = new Punto2D(98, 76);
+            Console.WriteLine(a.ToString());
+
+            Punto3D b;
+            b = new Punto3D(30, 21);
+
+            Console.WriteLine(b.ToString());
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Padre e hijo/Punto2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padre_e_hijo
+{
+    class Punto2D : Punto
+    {
+        private double y;
+
+        public double Y
+        {
+            get 
+            { 
+                return y; 
+            }
+            set 
+            { 
+                y = value;
+            }
+        }
+        public Punto2D() : base()
+        {
+            y = 0;
+        }
+        public Punto2D(double x, double y) : base(x)
+        {
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return base.ToString() +" Y="+y;
+        }
+    }
+}
